e5f3af55612a0104967fbc0ceb11057b16396156
ravi2025v <ravi2025v@gmail.com>
2026-06-19 15:02:45 +0530

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { AuthProvider } from './contexts/AuthContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { RoleRoute } from './components/ProtectedRoute';
 import { DashboardLayout } from './layouts/DashboardLayout';
-import { DbCheck } from './DbCheck';
 
 // Public
 import Home from './pages/Home';
